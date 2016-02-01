162a5f9ed05bf5375e80f02d5a1c22b93be427e7
Hagen Toennies <hagen.toennies@geektank.de>
2016-02-01 16:23:40 +0100

    wip

--- haskell-for-greater-good.hsproj/Haskell_for_greater_good.hs
+++ haskell-for-greater-good.hsproj/Haskell_for_greater_good.hs
@@ -1,3 +1,13 @@
 doubleMe x = x + x
 
 doubleUs x y = x*2 + y*2
+
+doubleSmallNumber x = if x > 100 
+                      then x 
+                      else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+conanO'Brien = "It’s a-me, Conan O'Brien!"
+
+lostNumbers = [4 ,8 ,15 ,16 ,23 ,48]
